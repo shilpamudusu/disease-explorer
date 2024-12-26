ae865c7cb6fecdd03cd000439496f1c480e63867
shilpamudusu <mudusushilpa@gmail.com>
2024-12-26 15:15:40 +0530

    completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-import { Button } from './components/ui/button'
 import DiseaseExplorer from './components/DiseaseExplorer'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
        <div className="min-h-screen bg-background">
